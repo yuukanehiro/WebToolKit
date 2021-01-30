7f53dc69076c8975e756c8acfdd25fd1f2517055
bravesoft-kanehiroyuu <y.kanehiro@bravesoft.co.jp>
2021-01-31 05:29:15 +0900

    アクセス情報表示ページリンク

--- src/resources/views/welcome.blade.php
+++ src/resources/views/welcome.blade.php
@@ -47,6 +47,7 @@
             <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
                 <div class="flex justify-center pt-8 sm:justify-start sm:pt-0">
                     ウェブツールズ
+                    <p><a href="/your-info">アクセス情報 【使用中のIPアドレス確認】</a></p>
                 </div>
 
                 <div class="mt-8 bg-white dark:bg-gray-800 overflow-hidden shadow sm:rounded-lg">
