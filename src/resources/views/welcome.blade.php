69ea357b542fc26d8fec4676e895f2b8691dda8b
bravesoft-kanehiroyuu <y.kanehiro@bravesoft.co.jp>
2021-01-31 22:23:59 +0900

    改行修正

--- src/resources/views/welcome.blade.php
+++ src/resources/views/welcome.blade.php
@@ -54,11 +54,12 @@
             <div class="col-md-6">
             <div class="card flex-md-row mb-4 shadow-sm h-md-250">
                 <div class="card-body d-flex flex-column align-items-start">
-                <strong class="d-inline-block mb-2 text-success">開発ツール</strong>
-                <h3 class="mb-0">
-                    <a class="text-dark" href="#">JSONツール(開発中)</a>
-                </h3>
-                <p class="card-text mb-auto">JSONに関する開発ツールです。開発中</a>
+                  <strong class="d-inline-block mb-2 text-success">開発ツール</strong>
+                  <h3 class="mb-0">
+                      <a class="text-dark" href="#">JSONツール(開発中)</a>
+                  </h3>
+                  <p class="card-text mb-auto">JSONに関する開発ツールです。開発中</a>
+                  <a href="#">ツールへ</a>
                 </div>
                 {{-- <img class="card-img-right flex-auto d-none d-lg-block" data-src="holder.js/200x250?theme=thumb" alt="Card image cap"> --}}
             </div>
