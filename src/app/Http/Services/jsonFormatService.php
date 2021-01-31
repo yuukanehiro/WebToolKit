ead4bbbbada1e1586c55c9f80eaa52dde61b6224
bravesoft-kanehiroyuu <y.kanehiro@bravesoft.co.jp>
2021-02-01 01:57:05 +0900

    コメント修正。デバッグ出力削除

--- src/app/Http/Services/jsonFormatService.php
+++ src/app/Http/Services/jsonFormatService.php
@@ -9,6 +9,7 @@ class JsonFormatService
     /**
      * JSONをきれいにして返却
      *
+     * @param string $json
      * @return array
      */
     public function formatJson(string $json): array
@@ -16,7 +17,6 @@ class JsonFormatService
         $array_response = json_decode($json, true);
         $json = json_encode($array_response, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT);
 
-        \Log::debug(json_encode($json));
         return $response = [
             'json' => $json
         ];
